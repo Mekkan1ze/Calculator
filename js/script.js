04107b9164f631932e2ae22e6d39d1ebc7712b32
Mekkan1ze <113686537+Mekkan1ze@users.noreply.github.com>
2023-01-24 10:49:56 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,34 +1,70 @@
-// тут в переменные мы записали наши кнопки.
-const resetButton = document.querySelector(".reset");
-const submitButton = document.querySelector(".submit");
-// получаем в переменную нашу форму и все данные которые в ней есть.
-// подробнее можно посмотреть тут https://learn.javascript.ru/form-elements
-const caloriesForm = document.forms.counter;
 
-// span с калориями
-const normalCal = document.querySelector(".normal_cal");
+	  var op; 
 
-// вешаем событие щелчка по кнопки внутри пример функции
-resetButton.addEventListener("click", (event) => {
-    alert("Reset!");
-
-    // нужно чтобы страница после нажатия не обновлялась
-    event.preventDefault();
-});
-
-submitButton.addEventListener("click", (event) => {
-    alert("Submit!");
-
-    // эту строчку можно будет убрать, она для теста
-    // пример того как можно получить значения в форме. У нас два инпута с именем gender
-    // поэтому внутри массива gender мы проверяем поле checked. Нажмите "Рассчитать" и проверьте
-    console.log(caloriesForm.gender[0].checked);
-
-    // тоже можно будет удалить, пример как менять верстку из JS
-    // нажмите "Рассчитать" и проверьте
-    normalCal.textContent = "999999999999";
-
-    // нужно чтобы страница после нажатия не обновлялась
-    event.preventDefault();
-});
-
+	  function func() {
+		  		event.preventDefault();
+	    var result;
+		var result1;
+		var result2;
+	    let age = Number(document.getElementById("age").value);
+	    let height = Number(document.getElementById("height").value);
+	    let weight = Number(document.getElementById("weight").value);
+	    switch (op) {
+	      case 'Man':
+	        gender = 5;
+	        break;
+	      case 'Women':
+	        gender = (-161);
+	        break;
+	    }
+		switch (op1) {
+		  case 'min':
+	        activity = 1.2;
+	        break;
+	      case 'low':
+	        activity = 1.375;
+	        break;
+		  case 'mid':
+	        activity = 1.55;
+	        break;
+		  case 'high':
+	        activity = 1.725;
+	        break;
+		  case 'veryhigh':
+	        activity = 1.9;
+	        break;
+	    }
+		weight1 = 10 * weight;
+		height1 = 6.25 * height;
+		age1 = 5 * age;
+		activity1 = (weight1 + height1 - age1) * activity;
+		activity2 = activity1 + gender;
+		plusves = activity2 + ((activity2 / 100 ) * 15);
+		minusves = activity2 - ((activity2 / 100 ) * 15);
+	    document.getElementById("result").innerHTML = Math.round(activity2);
+		document.getElementById("result1").innerHTML = Math.round(minusves);
+	    document.getElementById("result2").innerHTML = Math.round(plusves);
+		document.getElementById('hidden').hidden = false;
+	  }
+	   document.getElementById('hidden').hidden = true;
+	   document.getElementById('reset').disabled = true;
+	   document.getElementById('submit').disabled = true;
+	   function toggleButton()
+            {
+                let age = document.getElementById('age').value;
+                let height = document.getElementById('height').value;
+                let weight = document.getElementById('weight').value;
+				i = age + height + weight;
+				a = age * height * weight;
+				event.preventDefault();
+                if (i == 0) {
+                    document.getElementById('submit').disabled = true;
+                } else {
+                    document.getElementById('submit').disabled = false;
+					if (a == 0) {
+						document.getElementById('reset').disabled = true;
+					} else {
+						document.getElementById('reset').disabled = false;
+					}
+                }
+            }
